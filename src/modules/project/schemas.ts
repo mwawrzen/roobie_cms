84bcd562226f1098df779c78beee0a33357a89d2
mwawrzen <w.w@op.pl>
2025-11-29 18:41:21 +0100

    feat: create project types and body schemas

---
+++ src/modules/project/schemas.ts
@@ -0,0 +1,22 @@
+import { t } from "elysia";
+import { PROJECT_STATUS, projectStatuses } from "@schema";
+import { createEnumObject } from "@utils";
+
+export interface Project {
+  id: number;
+  name: string;
+  description: string| null;
+  apiKey: string;
+  status: PROJECT_STATUS;
+  createdAt: string;
+};
+
+export const CreateProjectBodySchema= t.Object({
+  name: t.String({ minLength: 1, maxLength: 255 }),
+  description: t.Optional( t.String() ),
+  status: t.Optional( t.Enum( createEnumObject( projectStatuses )))
+});
+
+export const UpdateProjectBodySchema= t.Partial( CreateProjectBodySchema );
+
+export type CreateProjectBody= ( typeof CreateProjectBodySchema )[ "static "];
