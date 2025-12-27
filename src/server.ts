d445e8f3832eb355fd6c56ecf4b56df1c790e94b
mwawrzen <w.w@op.pl>
2025-12-27 22:19:53 +0100

    feat: customize api metadata

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,15 @@ import { api } from "@/src/api/v1/index";
 const PORT= process.env.PORT!;
 
 const app= new Elysia()
-  .use( openapi() )
+  .use( openapi({
+    documentation: {
+      info: {
+        title: "Roobie CMS API",
+        version: "1.0.0",
+        description: "Documentation of Roobie CMS REST API"
+      }
+    }
+  }))
   .use( api );
 
 app.listen( PORT, ({ hostname, port })=> {
